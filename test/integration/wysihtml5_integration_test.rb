28669a7a1a9d3ddc15fc34b8adce03fa99147197
Michael Archibald <micharch54@gmail.com>
2012-03-26 10:17:10 -0600

    changed wysihtml5 to always have same name

--- test/integration/wysihtml5_integration_test.rb
+++ test/integration/wysihtml5_integration_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 describe "wysihtml5 rails integration" do
-  it "provides wysihtml5-0.3.0_rc1.min.js on the asset pipeline" do
-    visit '/assets/wysihtml5-0.3.0_rc1.min.js'
+  it "provides wysihtml5.js on the asset pipeline" do
+    visit '/assets/wysihtml5.js'
     page.text.must_include 'var wysihtml5='
   end
 end
